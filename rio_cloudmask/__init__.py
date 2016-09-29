6c72012f18cf058e8e69878cb14835926a08602d
Matthew Perry <perrygeo@gmail.com>
2016-09-29 17:34:52 -0400

    coverage

--- rio_cloudmask/__init__.py
+++ rio_cloudmask/__init__.py
@@ -7,6 +7,6 @@ __version__ = '0.2.0'
 # See # https://docs.python.org/2/howto/logging.html#configuring-logging-for-a-library
 class NullHandler(logging.Handler):
     def emit(self, record):
-        pass
+        pass  # pragma: no cover
 log = logging.getLogger(__name__)
 log.addHandler(NullHandler())
